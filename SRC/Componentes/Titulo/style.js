a7d897334934d444684f99e74f940324ca2406d1
DudinhaDaDz7 <eduardamsc27@gmail.com>
2024-11-07 10:20:43 -0300

    Título

---
+++ SRC/Componentes/Titulo/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+ BoxTitle: {
+ alignItems: "center",
+ },
+
+ texTitle: {
+ color: "#530f75",
+ backgroundColor: '#8b7ceb',
+ fontSize: 18,
+ fontWeight: 'bold',
+ borderRadius: 6,
+ borderColor: '#20232a',
+ borderWidth: 4,
+
+
+ },
+});
+
+export default styles
